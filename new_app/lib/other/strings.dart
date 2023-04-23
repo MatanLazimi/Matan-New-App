396ef05a606c3416d3a0376fc2b391d0447abc11
Matan-Lazimi-Mac <matanlazimi@gmail.com>
2023-04-23 20:30:43 +0300

    Add new constants and strings

---
+++ new_app/lib/other/strings.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class StringsAndConsts {
+  //Strings
+  static const String HOME_TITLE = 'Top Brokers';
+
+  //Consts
+  static const String MAIN_BUNDLE = 'Flutter Matan Demo';
+
+  //Colors
+  static Color? APPBAR_COLOR = Colors.grey[800];
+}
